857cc3731a48b3e836e49eaa31876be9663eddba
Max Penet <m@qbits.cc>
2017-11-30 14:13:32 +0100

    fix broken (insecure dependency)

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   :url "https://github.com/mpenet/tardis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["eaio.com" {:url "http://eaio.com/maven2/"
-                              :checksum :ignore}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.eaio.uuid/uuid "3.4"]]
+                 [com.github.stephenc.eaio-uuid/uuid "3.4.0"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev  {:dependencies []}
