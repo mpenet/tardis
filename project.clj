f3a25711f5ae7479785266f14080a86940b5a1e7
Max Penet <m@qbits.cc>
2017-11-30 14:14:00 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/tardis "1.1.0"
+(defproject cc.qbits/tardis "1.1.1"
   :description "Simple library to manage Type 1 UUIDs (time based)"
   :url "https://github.com/mpenet/tardis"
   :license {:name "Eclipse Public License"
